afbf637c1b6661e306c9a479759a8bb35f083170
Jacob Hinkston <jacob.hinkston@protonmail.com>
2024-08-05 22:18:00 -0700

    WIP - jacob sucks at CSS now lol

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,32 +1,37 @@
 import React from 'react'
+import './Home.css'
 
 export default function Home() {
   return (
-    <div className="page-home">
-        <h1>Catalyzing Health with AI</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit, sed do eiusmod tempor incididunt ut labore.</p>
+    <main className="home-page">
+        <section>
+            <h1>Catalyzing Health with AI</h1>
+            <p className="p-information">Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit, sed do eiusmod tempor incididunt ut labore.</p>
+        </section>
+        
         <h2> We are a company committed to wellness and Ai lorem
         ipsum I think Seamus will have the official text. </h2>
-        <div>
-            <h1>The Problem</h1>
-            <p>
+        <section className="home-the-problem">
+            <h1 id="theProblem">The Problem</h1>
+            <p className="p-information">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad minim.
             </p>
-            <div>
-                <h1>20%</h1>
-                <p>Say it’s a lack of knowledge</p>
-            </div>
-            <div>
-                <h1>23%</h1>
-                <p>Think it takes too much time</p>
-            </div>
-            <div>
-                <h1>40%</h1>
-                <p>Believe it’s too expensive</p>
-            </div>
-
-        </div>
-    </div>
+            <section>
+                <div className="the-problem-h1-section">
+                    <h1>20%</h1>
+                    <p className="p-information">Say it’s a lack of knowledge</p>
+                </div>
+                <div>
+                    <h1>23%</h1>
+                    <p className="p-information">Think it takes too much time</p>
+                </div>
+                <div>
+                    <h1>40%</h1>
+                    <p className="p-information">Believe it’s too expensive</p>
+                </div>
+            </section>
+        </section>
+    </main>
   )
 }
--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ ReactDOM.render(
   <React.StrictMode>
     <div className="App">
 
-      {/* <div>
+      <div>
           <main className="test-1">
       <iframe
         src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGM6iFXsFY&#x2F;n05TzU29exCOZ_EU7Kxk8g&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen"
@@ -42,7 +42,7 @@ ReactDOM.render(
       </iframe>
     </main>
     <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGM6iFXsFY&#x2F;n05TzU29exCOZ_EU7Kxk8g&#x2F;view?utm_content=DAGM6iFXsFY&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">New Website Design (as of 8.5)</a> by Sydney Jean-Simon
-      </div> */}
+      </div>
 
       
 
