08b1c67846211a556f2fbc559447f7bee7566f3f
Ryan Wittmers <rwittmers19@georgefox.edu>
2024-08-05 20:33:16 -0700

    Added Vision section.

---
+++ src/components/Vision/Vision.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Vision.css';
+
+const Vision = () => (
+  <section className="vision">
+    <div className="vision-content">
+      <div className="vision-text-left">
+        <h3 className="vision-subtitle">Our Vision</h3>
+        <h1 className="vision-title">
+          Full control of your grocery list, right at your fingertips.
+        </h1>
+      </div>
+      <div className="vision-text-right">
+        <p className="vision-description">
+          Lorem ipsum odor amet, consectetur adipiscing elit. At justo malesuada aliquet scelerisque dignissim iaculis arcu purus. Ultricies mauris ornare ut nec habitant arcu ex class conubia.
+        </p>
+      </div>
+    </div>
+  </section>
+);
+
+export default Vision;
