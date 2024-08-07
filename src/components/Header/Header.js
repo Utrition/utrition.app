70355b562cd7aba56aa64df92d3adb9005c15751
Ryan Wittmers <rwittmers19@georgefox.edu>
2024-08-06 18:50:48 -0700

    Add hashrouter

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
 import Button from '@mui/material/Button';
 import { ArrowForwardIos } from '@mui/icons-material';
 
-export default function Header({openModal}) {
+export default function Header() {
   return (
     <header className="header">
+      <h1 className="header-main-text">
+        UTRITION
+      </h1>
 
-        <h1 className="header-main-text">
-            UTRITION
-        </h1>
+      <nav>
+        {/* Use hash-based URLs for navigation */}
+        <a href="#/"><h3>Home</h3></a>
+        {/* <a href="#/vision"><h3>Vision</h3></a> */}
+        <a href="#/company"><h3>Company</h3></a>
+      </nav>
 
-        <nav>
-            <a href="/"><h3>Home</h3></a>
-            {/* <a href="/vision"><h3>Vision</h3></a> */}
-            <a href="/company"><h3>Company</h3></a>
-        </nav>
-        
-        <Button variant="contained" className="header-button">
-          <a href="https://forms.gle/ZEHKrHQyYDRX9GCB8">
-            Sign Up
-          </a>
-          <ArrowForwardIos className="header-button-icon"/>
-        </Button>
-
+      <Button variant="contained" className="header-button">
+        <a href="https://forms.gle/ZEHKrHQyYDRX9GCB8" target="_blank" rel="noopener noreferrer">
+          Sign Up
+        </a>
+        <ArrowForwardIos className="header-button-icon" />
+      </Button>
     </header>
-
-  )
-}   
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,43 +1,30 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { HashRouter, Routes, Route } from 'react-router-dom'; // Use HashRouter
 import reportWebVitals from './reportWebVitals';
 
 import Header from './components/Header/Header';
-import Home from './pages/Home/Home';
 import Vision from './pages/Vision/Vision';
 import Company from './pages/Company/Company';
 import Footer from './components/Footer/Footer';
 
 import './index.css';
 
-
 export default function App() {
-
-  const [isOpen, open] = useState(false);
 
   return (
     <div className="App">
-      <Header/>
-      <RouterProvider router={createBrowserRouter([
-        // {
-        //   path: "/",
-        //   element: <Home/>
-        // },
-        {
-          path: "/",
-          element: <Vision/>,
-        },{
-          path: "/company",
-          element: <Company />,
-        }
-      ])}/>
+      <Header />
+      <HashRouter> {/* Wrap the RouterProvider with HashRouter */}
+        <Routes>
+          {/* Define your routes here */}
+          <Route path="/" element={<Vision />} />
+          <Route path="/company" element={<Company />} />
+        </Routes>
+      </HashRouter>
       <Footer />
     </div>
-  )
+  );
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -46,11 +33,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 );
 
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-
