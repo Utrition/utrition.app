65ee80334c6e897cb5bd962f0cbbb4eccb9ae512
Ryan Wittmers <rwittmers19@georgefox.edu>
2024-11-01 10:19:57 -0700

    Replacing Google Form with Notion Form.

--- src/components/SignUpButton/SignUpButton.jsx
+++ src/components/SignUpButton/SignUpButton.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import { ArrowForwardIos } from '@mui/icons-material';
+import React from "react";
+import Button from "@mui/material/Button";
+import { ArrowForwardIos } from "@mui/icons-material";
 
-import './SignUpButton.css';
+import "./SignUpButton.css";
 
 export default function SignUpButton() {
-    return (
-        <Button variant="contained" className="signup-button">
-          <a href="https://forms.gle/ZEHKrHQyYDRX9GCB8" target="_blank" rel="noopener noreferrer">
-            Sign Up
-          </a>
-          <ArrowForwardIos className="signup-button-icon" />
-        </Button>
-    );
+  return (
+    <Button variant="contained" className="signup-button">
+      <a
+        href="https://utrition.notion.site/12fbcc38ac63802ea0b0c49950dc2512?pvs=105"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Sign Up
+      </a>
+      <ArrowForwardIos className="signup-button-icon" />
+    </Button>
+  );
 }
-
